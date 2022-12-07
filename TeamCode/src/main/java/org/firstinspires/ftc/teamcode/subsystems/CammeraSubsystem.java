732f9becb7f2c434d3677dcc5cb23555e1f7cc6d
Dylan Carter <dylan.carter@lhssc.org>
2022-12-06 19:23:20 -0600

    more work on the camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CammeraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CammeraSubsystem.java
@@ -29,56 +29,85 @@ public class CammeraSubsystem extends SubsystemBase {
 
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
-    private TFObjectDetector initTfod;
-    initVuforia();
+    List<Recognition> updatedRecognitions;
+
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.75f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 300;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+
+        // Use loadModelFromAsset() if the TF Model is built in as an asset by Android Studio
+        // Use loadModelFromFile() if you have downloaded a custom team model to the Robot Controller's FLASH.
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+        // tfod.loadModelFromFile(TFOD_MODEL_FILE, LABELS);
+    }
 
 
+    public CammeraSubsystem() {
+        initVuforia();
+        initTfod();
+    }
 
-    public CammeraSubsystem(TFObjectDetector ){
-        if (tfod != null) {
-            tfod.activate();
-
-            // The TensorFlow software will scale the input images from the camera to a lower resolution.
-            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
-            // If your target is at distance greater than 50 cm (20") you can increase the magnification value
-            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
-            // should be set to the value of the images used to create the TensorFlow Object Detection model
-            // (typically 16/9).
-            tfod.setZoom(1.0, 16.0/9.0);
-        }
-
-        if (opModeIsActive()) {
-            while (opModeIsActive()) {
-                if (tfod != null) {
-                    // getUpdatedRecognitions() will return null if no new information is available since
-                    // the last time that call was made.
-                    public void Updatecameravalues{
-                        List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                    }
-                    if (updatedRecognitions != null) {
-                        telemetry.addData("# Objects Detected", updatedRecognitions.size());
-
-                        // step through the list of recognitions and display image position/size information for each one
-                        // Note: "Image number" refers to the randomized image orientation/number
-                        for (Recognition recognition : updatedRecognitions) {
-                            double col = (recognition.getLeft() + recognition.getRight()) / 2 ;
-                            double row = (recognition.getTop()  + recognition.getBottom()) / 2 ;
-                            double width  = Math.abs(recognition.getRight() - recognition.getLeft()) ;
-                            double height = Math.abs(recognition.getTop()  - recognition.getBottom()) ;
-
-
-                            telemetry.addData(""," ");
-                            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100 );
-                            telemetry.addData("- Position (Row/Col)","%.0f / %.0f", row, col);
-                            telemetry.addData("- Size (Width/Height)","%.0f / %.0f", width, height);
-                        }
-                        telemetry.update();
-                    }
-                }
+    public void Updatecameravalues() {
+        updatedRecognitions = tfod.getUpdatedRecognitions();
+        if (updatedRecognitions != null) {
+             telemetry.addData("# Objects Detected", updatedRecognitions.size());
+            for (Recognition recognition : updatedRecognitions) {
+                telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100 );
             }
-//    public void periodic(){
+        }
+        else{
+            telemetry.addLine("camera is called but null");
+        }
+    }
+
+    public void periodic(){
+        Updatecameravalues();
+    }
+
+//            public void
+//                if (tfod != null) {
+//                    // getUpdatedRecognitions() will return null if no new information is available since
+//                    // the last time that call was made.
 //
-//    }
-
-
+//
+//
+//                        // step through the list of recognitions and display image position/size information for each one
+//                        // Note: "Image number" refers to the randomized image orientation/number
+//                        for (Recognition recognition : updatedRecognitions) {
+//                            double col = (recognition.getLeft() + recognition.getRight()) / 2 ;
+//                            double row = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+//                            double width  = Math.abs(recognition.getRight() - recognition.getLeft()) ;
+//                            double height = Math.abs(recognition.getTop()  - recognition.getBottom()) ;
+//
+//
+//                            telemetry.addData(""," ");
+//                            telemetry.addData("- Position (Row/Col)","%.0f / %.0f", row, col);
+//                            telemetry.addData("- Size (Width/Height)","%.0f / %.0f", width, height);
+//                        }
+//                        telemetry.update();
+//                    }
 }
